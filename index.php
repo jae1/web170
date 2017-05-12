eee5066505825cc8d66d5650e8f3cc9c14137023
Jaewon Jeong <7904001@gmail.com>
2017-05-11 17:42:23 -0700

    06) Create Your Main & Sub Menu Systems

--- index.php
+++ index.php
@@ -35,12 +35,9 @@
 				<span class="line"></span>
 				<span class="line"></span>
 			</div>
-			<ul id="nav-items">
-				<li><a href="about">ABOUT</a></li>
-				<li><a href="menu">MENU</a></li>
-				<li><a href="blog">BLOG</a></li>
-				<li><a href="contact">CONTACT</a></li>
-			</ul>
+			<!-- Begin WordPress Menu -->
+			<?php wp_nav_menu(array('theme_location' => 'main-menu', 'container' => false, 'menu_class' => false,)); ?>
+			<!-- End WordPress Menu -->
 		</nav>
 		<!-- END NAVIGATION -->
 	</header>
@@ -76,14 +73,27 @@
 
 			<!-- SIDEBAR -->
 			<aside id="sidebar">
-				<ul>
-					<li><span class="active">About</span>
-						<ul>
-							<li><a href="story">Story</a></li>
-							<li><a href="mission">Mission</a></li>
-						</ul>
-					</li>
-				</ul>
+				<?php
+					echo '<h2>';
+					if (is_page()) {
+						echo get_the_title($post->post_parent);
+					} else {
+						echo 'Blog';
+					}
+					echo '</h2>';
+
+					echo '<ul>';
+					if (is_page() || is_404()) { // when on a page
+						if ($post->post_parent) { // the page has a parent
+							wp_list_pages(array('title_li' => '', 'child_of' => $post->post_parent,)); // list the children
+						} else { // when on a parent Page
+							wp_list_pages(array('title_li' => '', 'child_of' => $post->ID,));
+						}
+					} else { // when not on a page
+						wp_list_categories(array('title_li' => '',)); // list blog categories
+					}
+					echo '</ul>';	
+				?>
 			</aside>
 		</div>
 	</div>
