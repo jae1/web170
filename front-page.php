679b77178c6882630fc4d4910b9cb3ac9ae793ef
Jaewon Jeong <7904001@gmail.com>
2017-05-16 17:06:06 -0700

    08) Create a Static Home Page with Multiple Loops for Your Site

--- front-page.php
+++ front-page.php
@@ -1,75 +1,38 @@
 <?php get_header(); ?>
 
-<!-- START BANNER -->
-<div id="banner-container">
-	<div id="banner-contents">
-		<h1 id="banner-title">EAT REAL FOOD</h1>
-		<a href="menu">SEE MENU</a>
-	</div>
-	<div class="arrow">
-		<span class="fa fa-arrow-down fa-2x"></span>
-	</div>
-</div>
-<!-- END BANNER -->
+<div id="full-h" class="container">
+	<div class="content">
+		<div id="main-content">
+			<h2>About Us</h2>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<article id="article-<?php the_ID(); ?>" class="article">
+					<?php the_content(); ?>
+				</article>
+			<?php endwhile; endif; ?>
+			
+			<h2>Latest News</h2>
+			<ul>
+			<?php rewind_posts(); ?>
+			<?php query_posts(array('posts_per_page' => '4', 'category_name' => 'news')); ?>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<li><a href="<?php the_permalink();?>"><?php the_title();?></a><br />
+				<small><?php the_time('F j, Y'); ?> in <?php the_category(','); ?></small></li>
+			<?php endwhile; endif; ?>
+			</ul>
+			
+			<h2>Latest Promotions</h2>
+			<ul>
+			<?php rewind_posts(); ?>
+			<?php query_posts(array('posts_per_page' => '4', 'category_name' => 'promotions')); ?>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<li><a href="<?php the_permalink();?>"><?php the_title();?></a><br />
+				<small><?php the_time('F j, Y'); ?> in <?php the_category(','); ?></small></li>
+			<?php endwhile; endif; ?>
+			</ul>
 
-<!-- START CONTAINER -->
-<div class="container main">
-	<!-- START CONTENT -->
-	<div class="content home">
-		<div class="description">
-			<h2>EXPERIENCE THE BEST FOOD</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper neque neque, at pretium arcu aliquam sed.</p>
-			<a href="menu">SEE MENU</a>
+			<small>front-page.php</small>
 		</div>
-		<img class="description-img" src="<?=bloginfo('template_directory')?>/images/menu.png" alt="menu" />
 	</div>
-	<!-- END CONTENT -->
 </div>
-<!-- END CONTAINER -->
 
-<!-- START CONTAINER -->
-<div class="container main">
-	<!-- START CONTENT -->
-	<div class="content home">
-		<div class="description">
-			<h2>FIND US</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper neque neque, at pretium arcu aliquam sed.</p>
-			<a href="contact">FIND US</a>
-		</div>
-		<img class="description-img" src="<?=bloginfo('template_directory')?>/images/truck.png" alt="contact" />
-	</div>
-	<!-- END CONTENT -->
-</div>
-<!-- END CONTAINER -->
-
-<!-- START CONTAINER -->
-<div class="container main">
-	<!-- START CONTENT -->
-	<div class="content home">
-		<div class="description">
-			<h2>WHAT'S NEW</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper neque neque, at pretium arcu aliquam sed.</p>
-			<a href="blog">SEE MORE</a>
-		</div>
-		<img class="description-img" src="<?=bloginfo('template_directory')?>/images/blog.png" alt="blog" />
-	</div>
-	<!-- END CONTENT -->
-</div>
-<!-- END CONTAINER -->
-
-<!-- START CONTAINER -->
-<div class="container main">
-	<!-- START CONTENT -->
-	<div class="content home">
-		<div class="description">
-			<h2>OUR STORY</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper neque neque, at pretium arcu aliquam sed.</p>
-			<a href="about">SEE MORE</a>
-		</div>
-		<img class="description-img" src="<?=bloginfo('template_directory')?>/images/about.png" alt="about" />
-	</div>
-	<!-- END CONTENT -->
-</div>
-<!-- END CONTAINER -->
-<small>front-page.php</small>
 <?php get_footer(); ?>
