eae8122761c4c200ab74861fe507792e05f744db
jae1 <7904001@gmail.com>
2017-06-02 12:05:40 -0700

    12) Search Engine Optimize Your Site

--- header.php
+++ header.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<!-- META -->
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="description" content="<?php echo get_the_excerpt(); ?>" />
 
-		<title><?=bloginfo('description')?> | <?=bloginfo('name')?></title>
+		<title><?php get_title_tag(); ?></title>
 
 		<!-- STYLES -->
-		<link href="<?=bloginfo('template_directory')?>/nav.css" rel="stylesheet" />
-		<link href="<?=bloginfo('stylesheet_url')?>" rel="stylesheet" />
-		<link href="<?=bloginfo('template_directory')?>form.css" rel="stylesheet" />
+		<link href="<?php bloginfo('template_directory'); ?>/nav.css" rel="stylesheet" />
+		<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" />
+		<link href="<?php bloginfo('template_directory'); ?>/form.css" rel="stylesheet" />
 		<!-- <link href="css/font-awesome.min.css" rel="stylesheet" /> -->
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
 
 		<!-- SCRIPTS -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		<script src="<?=bloginfo('template_directory')?>/js/nav.js"></script>
+		<script src="<?php bloginfo('template_directory'); ?>/js/nav.js"></script>
 		<script src="https://use.fontawesome.com/3f2fee1521.js"></script>
 		
 		<!-- Begin WP Head function -->
-		<?=wp_head()?>
+		<?php wp_head(); ?>
 		<!-- End WP Head function -->
 	</head>
 
@@ -35,6 +37,7 @@
 					<span class="line"></span>
 					<span class="line"></span>
 				</div>
+				
 				<!-- Begin WordPress Menu -->
 				<?php wp_nav_menu(array('theme_location' => 'main-menu', 'container' => false, 'menu_class' => false,)); ?>
 				<!-- End WordPress Menu -->
@@ -42,3 +45,12 @@
 			<!-- END NAVIGATION -->
 		</header>
 		<!-- END HEADER -->
+		<?php if (!is_front_page()) { ?>
+		<!-- START PAGE TITLE -->
+		<div class="container page-title">
+			<div id="page-title">
+				<h2><?php if (is_category()) { single_cat_title(); } else { single_post_title(); } ?></h2>
+			</div>
+		</div>
+		<?php } ?>
+		<!-- END PAGE TITLE -->
